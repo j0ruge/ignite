329fdc2900dfe60671b8d3f2113d1dcdfc30ec6d
JorUge <joruge@gmail.com>
2021-11-10 22:19:14 -0300

    Inicio do projeto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { request, response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.listen(3333);
+app.get("/", (request, response) => {
+  return response.json({message: "Hello World Ignite - Fundamentos NodeJS!"});
+});
