fc3ff7ed6a380d86427d6c1c744fbdcc42b2000d
JorUge <joruge@gmail.com>
2021-11-11 15:11:35 -0300

    Final do primeiro módulo

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+  const query = request.query;  
+  console.log(query);  
+  return response.json(["Curso 01", "Curso 02", "Curso 03"]);
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  return response.json(["Curso 01", "Curso 02", "Curso 03", "Curso 04"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  const params = request.params;
+  const { id } = params;
+  console.log(params);
+  console.log(`O ID utilizado foi: ${id}`);
+  return response.json(["Curso 06", "Curso 02", "Curso 03", "Curso 04"]);
+} );
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 06", "Curso 07", "Curso 03", "Curso 04"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 06", "Curso 07", "Curso 04"]);
+})
 
 app.listen(3333);
-app.get("/", (request, response) => {
-  return response.json({message: "Hello World Ignite - Fundamentos NodeJS!"});
-});
