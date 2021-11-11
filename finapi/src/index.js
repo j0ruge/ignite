b259f5f30bc461017a8d4c6254db12288404c230
JorUge <joruge@gmail.com>
2021-11-11 19:04:57 -0300

    CPF enviado como Header e primeiro Middleware implementado

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -6,6 +6,17 @@ const app = express();
 app.use(express.json());
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next){
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+  if(!customer){
+    return response.status(400).json({error: "Costumer not found"})
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExist = customers.some(
@@ -27,12 +38,8 @@ app.get("/account", (request, response) => {
   return response.status(200).send(customers);
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
-  const customer = customers.find(customer => customer.cpf === cpf);
-  if(!customer){
-    return response.status(400).json({error: "Costumer not found"})
-  }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {  
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
