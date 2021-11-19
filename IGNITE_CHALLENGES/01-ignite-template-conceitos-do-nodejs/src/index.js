9a4bbd3140eb9e63efc11303c1722676f29c530a
JorUge <joruge@gmail.com>
2021-11-18 22:41:21 -0300

    Desafio 02 - Midlewares entregue

--- IGNITE_CHALLENGES/01-ignite-template-conceitos-do-nodejs/src/index.js
+++ IGNITE_CHALLENGES/01-ignite-template-conceitos-do-nodejs/src/index.js
@@ -14,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
   const user = users.find(user => user.username === username);
   if (!user) {
-    return response.status(400).json({error: "User not found"})
+    return response.status(404).json({error: "User not found"})
   }
   request.user = user;
   return next();
@@ -75,16 +75,13 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { id } = request.params;
   const { title, deadline } = request.body;
-  const { user } = request;  
-    
-
+  const { user } = request;
   const idAlreadyExist = user.todos.some(
     (todo) => todo.id === id
   );
   if (!idAlreadyExist) {
     return response.status(404).json({error: "ID not found"});
   }
-
   const todo = checksExistTodo(user, id);
   todo.title = title;
   todo.deadline = deadline;  
