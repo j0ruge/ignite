5078d9a391078d559f73c2eed4b91d201e299463
JorUge <joruge@gmail.com>
2021-11-25 18:10:11 -0300

    Criando categoria

--- RENTALX/src/server.ts
+++ RENTALX/src/server.ts
@@ -1,9 +1,10 @@
-import express, { request, response } from 'express';
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  console.log({ message: 'Hello World' });
-});
+app.use(categoriesRoutes);
 
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(3333, () => console.log("Server is running!"));
