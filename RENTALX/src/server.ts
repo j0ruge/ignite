d0c46516877cb51b7bc645c6a0b7471cb036cad0
JorUge <joruge@gmail.com>
2021-11-25 15:56:45 -0300

    boilerplate

--- RENTALX/src/server.ts
+++ RENTALX/src/server.ts
@@ -2,9 +2,8 @@ import express, { request, response } from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log({ message: 'Hello World' });
+});
 
-app.get("/", (request, response) => {
-  console.log({message: "Hello World"});  
-})
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => console.log('Server is running!'));
