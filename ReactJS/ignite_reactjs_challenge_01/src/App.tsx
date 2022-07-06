04fb508bd2851d3b4a3def5507cd14400cd53d5b
j0ruge <46424602+j0ruge@users.noreply.github.com>
2022-07-05 23:16:49 -0300

    Final do desafio ignite react 02

--- ReactJS/ignite_reactjs_challenge_01/src/App.tsx
+++ ReactJS/ignite_reactjs_challenge_01/src/App.tsx
@@ -1,9 +1,11 @@
 import { TaskList } from './components/TaskList'
 import { Header } from "./components/Header";
 import './styles/global.scss'
+import { useState } from 'react';
 
 
 export function App() {
+  
   return (
     <>
       <Header />
