8ed4c3343c3288a1a0c51728449b57d1769248ef
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-05-26 18:34:10 -0300

    02. Salvando usuários em memória (Headers)

--- Node.js/2023/01-fundamentos-nodejs/src/server.js
+++ Node.js/2023/01-fundamentos-nodejs/src/server.js
@@ -1,17 +1,25 @@
 import http from 'node:http';
 
 
+const users = [];
+
 const server = http.createServer((request, response) => {
     const {method, url} = request;
 
     console.log(`Request method: ${method} | Request URL: ${url}`);
 
     if (method === 'GET' && url === '/users') {
-        return response.end('Listagem de usuários');
+        return response
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users));
     }
 
     if (method === 'POST' && url === '/users') {
-        return response.end('Criação de usuário');
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+        });
     }    
 
     return response.end('Hello Ignite!');
