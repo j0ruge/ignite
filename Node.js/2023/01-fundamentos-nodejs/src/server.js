8144306a926c10650bc7d30c9c0196d48c44635b
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-05-26 18:18:24 -0300

    Rotas de criação e listagem (Métodos HTTP)

---
+++ Node.js/2023/01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,20 @@
+import http from 'node:http';
+
+
+const server = http.createServer((request, response) => {
+    const {method, url} = request;
+
+    console.log(`Request method: ${method} | Request URL: ${url}`);
+
+    if (method === 'GET' && url === '/users') {
+        return response.end('Listagem de usuários');
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return response.end('Criação de usuário');
+    }    
+
+    return response.end('Hello Ignite!');
+});
+
+server.listen(3333); // localhost:3333
