3e15d1392ea0d3bc460f94d658f27c7cb670560f
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-05-29 18:49:56 -0300

    01. Separando rotas da aplicação

--- Node.js/2023/01-fundamentos-nodejs/src/server.js
+++ Node.js/2023/01-fundamentos-nodejs/src/server.js
@@ -1,10 +1,7 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
-import { Database } from './database/database.js';
+import { routes } from './routes/routes.js';
 
-
-const database = new Database;
 
 const server = http.createServer(async (request, response) => {
     const {method, url} = request;
@@ -13,21 +10,13 @@ const server = http.createServer(async (request, response) => {
 
     await json(request, response);
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users');
-        return response.end(JSON.stringify(users));
-    }
+    const route = routes.find(route => {
+        return route.method === method && route.url === url
+    });
 
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = request.body;
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        };
-        database.insert('users', user);
-        return response.writeHead(201).end();
-    }    
+    if (route) {
+        return route.handler(request, response);
+    }
 
     return response.writeHead(404).end();
 });
