1bc43891dd9ac938eca2fb93d75745ec33f26a6f
JORGE FERRARI <joruge@gmail.com>
2023-06-11 22:14:42 -0300

    05. Configurando ESLint

---
+++ Node.js/Trilha 2023/02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', async (request, reply) => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running on port 3333!')
+  })
