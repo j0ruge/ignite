0e358dfe55537f5373e4c985aa3263c2d7476392
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-06-14 17:47:12 -0300

    03. Criação de transações

--- Node.js/Trilha 2023/02-api-rest-nodejs/src/routes/transactions.ts
+++ Node.js/Trilha 2023/02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,29 +1,26 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-import crypto from 'node:crypto'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/inserir', async () => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 1000,
-      })
-      .returning('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transaction
-  })
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
-  app.get('/select', async () => {
-    const transaction = await knex('transactions').select('*')
-    return transaction
-  })
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
 
-  app.get('/filter', async () => {
-    const transaction = await knex('transactions')
-      .where('amount', '1000')
-      .select('*')
-    return transaction
+    return reply.status(201).send()
   })
 }
