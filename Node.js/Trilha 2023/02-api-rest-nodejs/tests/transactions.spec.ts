0f262e658fc3d72fa49d468514c3633d1d76e6a0
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-06-15 15:21:29 -0300

    05. Testando listagem de transações

--- Node.js/Trilha 2023/02-api-rest-nodejs/tests/transactions.spec.ts
+++ Node.js/Trilha 2023/02-api-rest-nodejs/tests/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest'
+import { expect, beforeAll, afterAll, describe, it } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -12,7 +12,7 @@ describe('Transactions', () => {
   })
 
   it('user should be able to create a new transaction', async () => {
-    await request(app.server)
+    const response = await request(app.server)
       .post('/transactions')
       .send({
         title: 'New transaction',
@@ -20,5 +20,31 @@ describe('Transactions', () => {
         type: 'credit',
       })
       .expect(201)
+
+    console.log(response.get('set-cookie'))
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookie = createTransactionResponse.get('set-cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('cookie', cookie)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
