a7e157e09f6f033e7f664a1733cbcae474292bbe
JORGE FERRARI <joruge@gmail.com>
2023-06-14 23:55:57 -0300

    02. Criando primeiro teste usando vitest

---
+++ Node.js/Trilha 2023/02-api-rest-nodejs/src/test/example.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from 'vitest'
+
+test('should be able to criate a new transaction', async () => {
+  const resposeStatusCode = 201
+
+  expect(resposeStatusCode).toEqual(201)
+})
