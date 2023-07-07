b133135f7ec1d4abeb7ab48b32f7239c1d09c102
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-07-07 16:35:50 -0300

    01. Fundamentos do Prisma ORM

--- Node.js/Trilha 2023/03-api-solid/src/app.ts
+++ Node.js/Trilha 2023/03-api-solid/src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify({ logger: true })
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'John',
+    email: 'doe@teste.com',
+  },
+})
