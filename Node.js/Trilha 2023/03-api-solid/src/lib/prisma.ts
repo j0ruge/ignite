e4ae55d7b2783c63632826d490c4c06ff6a8d089
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-07-12 15:40:29 -0300

    Update

--- Node.js/Trilha 2023/03-api-solid/src/lib/prisma.ts
+++ Node.js/Trilha 2023/03-api-solid/src/lib/prisma.ts
@@ -1,3 +1,8 @@
+import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
+
+export { prisma }
