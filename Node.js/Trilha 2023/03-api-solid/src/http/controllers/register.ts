e7660b6f1bb8ff12ac99975d728ae604c1f08c41
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-07-13 18:29:44 -0300

    08. Lidando com erros do use case

--- Node.js/Trilha 2023/03-api-solid/src/http/controllers/register.ts
+++ Node.js/Trilha 2023/03-api-solid/src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registrBodySchema = z.object({
@@ -18,7 +19,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+    return reply.status(500).send() // TODO: improve error handling
   }
 
   return reply.status(201).send()
