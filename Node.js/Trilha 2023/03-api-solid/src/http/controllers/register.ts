d6aa5b1ca4dbb91687930a55088a962d79b25357
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-07-13 18:52:35 -0300

    09. Handler de erros global

--- Node.js/Trilha 2023/03-api-solid/src/http/controllers/register.ts
+++ Node.js/Trilha 2023/03-api-solid/src/http/controllers/register.ts
@@ -22,7 +22,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(400).send({ message: error.message })
     }
-    return reply.status(500).send() // TODO: improve error handling
+    throw error
   }
 
   return reply.status(201).send()
