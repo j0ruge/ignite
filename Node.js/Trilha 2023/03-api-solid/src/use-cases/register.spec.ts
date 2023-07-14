5d9f8d7a6d5ae429428c69f98bb355d23937ffb1
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-07-14 10:34:57 -0300

    02. Primeiro teste unitário

--- Node.js/Trilha 2023/03-api-solid/src/use-cases/register.spec.ts
+++ Node.js/Trilha 2023/03-api-solid/src/use-cases/register.spec.ts
@@ -1,5 +1,33 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if 1 + 1 = 2', () => {
-  expect(1 + 1).toBe(2)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return Promise.resolve(null)
+      },
+      async create(data) {
+        return {
+          id: 'any_id',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+          updated_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordHashValid = await compare('123456', user.password_hash)
+
+    expect(isPasswordHashValid).toBe(true)
+  })
 })
